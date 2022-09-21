cff439fc583ec5108cc667499bb618fd7ca64832
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-09-21 11:48:58 +0530

    hangma task

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css"
 var guess;
 var check ;
 var a ;
+var empty=[];
 
 
 
@@ -13,19 +14,12 @@ const App = () => {
   const inputref = useRef(['A' , 'B' ,'C' ,'D' ,'E' ,'F' ,'G' ,'H' ,'I' ,'J' ,'K' ,'L' ,'M' ,'N' ,'O' ,'P' ,'Q' ,'R' ,'S' ,'T' ,'U' ,'V' ,'W' ,'X' ,'Y' ,'Z'])
   const buttonref= useRef([])
   const[words , setWords]=useState(['BATMAN','SUPERMAN','SHAKTIMAAN','THOR','SPIDERMAN','HULK' ,'FLASH' , 'KRISH' , 'CAPTIANAMERICA' , 'WONDERWOMAN', 'AQUAMAN','BLACKPANTHER'  ])
-  const[hint ,setHint]=useState(['I am from Gotham' ,'I wear Underwear outside my trouser' ,'Andhera kayam rahe' ,'Where is my Hammer?','I am not feeling good , Mr. stark' ,'Smash','Faster than light' , 'Aila complan' ,'How old are you?' ,'lasso of truth' , 'King of Atlanta' ,'Wakanda Forever'])
+  const[hint ,setHint]=useState(['I am from Gotham' ,'I wear Underwear outside my trouser' ,'Andhera kayam rahe' ,'Where is my Hammer?','I am not feeling good , Mr. stark' ,'Smash','Faster than light' , 'Mai aa raha hu maa , meri shaktiyonn ka galat istemal hua hai' ,'How old are you?' ,'lasso of truth' , 'King of Atlanta' ,'Wakanda Forever'])
   const[realword , setRealword] = useState([])
   const[arrword , setArrword] = useState([])
   const[flag , setFlag]=useState(0)
   const[count , setCount]=useState(0)
   const[imgarr , setImgarr]=useState(["./images/1.png" , "./images/2.png" , "./images/3.png" ,"./images/4.png" , "./images/5.png"  , "./images/6.png" , "./images/7.png" ])
- 
-  // const start_fun=()=>{
-  //   a = Math.floor(Math.random()*11);
-  //   guess = words[a].split("")
-  //   setArrword(guess)
-  //   setFlag(1)  
-  // }
   useEffect(()=>{
     a = Math.floor(Math.random()*11);
     guess = words[a].split("")
@@ -38,42 +32,28 @@ const App = () => {
     var a = e.target.innerText
     arrword.map((d , i)=>{
       if(d === a){
-
         check = true
+        empty.push(i)
         realword[i] = a;
-        setRealword([...realword])
         buttonref.current[iref].style.color="white"
         buttonref.current[iref].style.backgroundColor="green"
         buttonref.current[iref].setAttribute('disabled' , true)
       }      
     })
-    console.log(realword)
-    if(check === false){
+    setRealword([...realword])
+      if(check === false){
       buttonref.current[iref].style.color="white"
       buttonref.current[iref].style.backgroundColor="red"
       buttonref.current[iref].setAttribute('disabled' , true)
      setCount(count+1)
     } 
 
-    if(realword.length === guess.length){
-      // alert('length')
+    if(guess.length === empty.length){
       setFlag(3)
     }
-   
   }
 
   const restart_fun=()=>{
-    // buttonref.current.map((item , i)=>{ 
-    //   buttonref.current[i].style.color="black"
-    //   buttonref.current[i].style.backgroundColor="white"
-    //   buttonref.current[i].setAttribute('disabled' , false)
-    // })
-    // setRealword([])
-    // setCount(0)
-    // a = Math.floor(Math.random()*6);
-    // guess = words[a].split("")
-    // setArrword(guess)
-    // setFlag(1)
     location.reload();
   }
 
@@ -84,14 +64,9 @@ const App = () => {
      }
   }, [count])
 
-  // useEffect(()=>{
-  //   console.log("check")
-  // })
-
   return (
     <div className="App">
       <div id="heading_div_id">HANGMAN</div>
-      {/* {flag===0?<button id="start_btn" onClick={start_fun}>START YOUR GAME</button>:""} */}
       <div id="div_lives_id">{flag===1?"Lives " + (7 - count):""}</div>
       <div id="img_div_id"><div>{flag===1?<img src ={imgarr[count]} alt="" />:""}</div>
       <div id="title">{flag===1?hint.map((d , i)=>{
@@ -107,7 +82,7 @@ const App = () => {
       <div>
       {flag===1?arrword.map((d , i)=>{
         return (
-          <input key={i} type="text"  className="input_box" value={realword[i]===undefined?"#":realword[i]} readOnly/>
+          <input key={i} type="text"  className="input_box" value={realword[i]===undefined?" ":realword[i]} readOnly/>
         )
       }):""}
       </div>
